3e22266039756a0e98f39181c037a51c7983c032
Marcio Bera <marciobera@hotmail.com>
2024-03-10 23:37:35 +0000

    Add database config using gorm

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/marciobera/go-simple-trivia-rest-api/database"
+)
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
